a2226ce373af222dd643d1cd6dac31b715ee6398
cmpowers1981 <c.m.powers@outlook.com>
2019-07-10 11:58:25 -0400

    Completed drill

--- Obj1-Loaf.js
+++ Obj1-Loaf.js
@@ -1,1 +1,18 @@
 
+//Drill 1 of Object Drills
+'use strict';
+
+const loaf = {
+    flour: '6 level cups',
+    water: '1.5 cups',
+    yeast: '1 packet, or 2 tsp',
+    salt: '1.5 tsp',
+    sugar: '1 pinch to yeast',
+
+    recipe: function() {
+        return(this.water / this.flour / this.yeast / this.salt / this.sugar)
+    }
+};
+
+console.log(loaf.flour, loaf.water, loaf.yeast, loaf.salt, loaf.sugar)
+console.log(loaf.recipe)
