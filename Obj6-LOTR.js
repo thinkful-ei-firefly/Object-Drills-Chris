3733c48c35caa9665293b72c450384dabbead618
cmpowers1981 <c.m.powers@outlook.com>
2019-07-10 17:37:01 -0400

    Submitting partially finished work due to solitary workload

--- Obj6-LOTR.js
+++ Obj6-LOTR.js
@@ -1,1 +1,28 @@
 
+//Object drill 6 - LOTR Character Creation
+'use strict';
+
+const characterSet = function(characterSet) {
+    return {
+        name: characterObj.name,
+        shortName: characterObj.firstName,
+        race: characterObj.race,
+        placeOfOrigin: characterObj.origin
+        atk: characterObj.attack,
+        def: characterObj.defense,
+        describe: function() {
+            return `${this.name} is a ${this.race} from ${this.origin}.`;
+        },
+        fightResults: function(foe) {
+            const defenseStrength = this.defense - foe.attack;
+            const attackStrength = this.attack - foe.defense;
+                if (attackStrength > 0) {
+            return `We emerge VICTORIOUS and takes ${attackStrenght} damage and you receive ${defenseStrenght} > 0 ? 0 : Math.abs(defenseStrength)} damage`;
+                }
+                else {
+            return `You wish you could defeat me!' you deliver 0 damamge and take $[defenseAttack > 0 ? 0 : Math.abs(defenseAttack)] damage` ;
+            }
+        
+        }
+    }
+}
