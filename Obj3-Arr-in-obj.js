fa38576c1c64d00195a2dd4077e0785e4050f748
cmpowers1981 <c.m.powers@outlook.com>
2019-07-10 14:16:06 -0400

    Completed task

--- Obj3-Arr-in-obj.js
+++ Obj3-Arr-in-obj.js
@@ -1,1 +1,9 @@
 
+// Object 3 Arrays in objects
+'use strict';
+//Hobbit quote about the type of meals missed
+const obj = {
+    meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
+};
+console.log(obj.meals[5]);
+
