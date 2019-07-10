88a291d26b3767552d2132764c708ca645d12af7
cmpowers1981 <c.m.powers@outlook.com>
2019-07-10 13:04:30 -0400

    Does not run properly, will tinker with later

--- Obj2-iterating.js
+++ Obj2-iterating.js
@@ -1,1 +1,19 @@
 
+//Drill 2 of Object Drills
+'use strict';
+
+const obj = {
+    foo: 20,
+    bar: 45,
+    fum: 'Fee fi fo',
+    quux: 'I smell the blood of a newbie coder',
+    Spam: 93,
+};
+
+function loop(obj) {
+    for (const key in obj) {
+        console.log(key, obj[key]);
+    }
+}
+//loop(obj);
+
