389c59caf9d760316453086a69b8bae1e046ddbb
cmpowers1981 <c.m.powers@outlook.com>
2019-07-11 10:28:43 -0400

    Completed LOTR Drill 6 of Object Drills

--- Obj6-LOTR.js
+++ Obj6-LOTR.js
@@ -1,7 +1,7 @@
 
 //Object drill 6 - LOTR Character Creation
 'use strict';
-
+/*
 const characterSet = function(characterSet) {
     return {
         name: characterObj.name,
@@ -51,8 +51,7 @@ let charAttribs = [
         placeOfOrigin:  'Thorin`s Hall',
         atk:             18,
         def:             22
-},
-{
+},{
     name:               'Aragorn Son of Arathorn',
         shortName:      'Aragorn',
         race:           'Human - Ranger',
@@ -70,4 +69,55 @@ let charAttribs = [
 
     },
 ]
+*/
 
+function Character(name, shortName, race, origin, attack, defense) {
+    this.name = name;
+    this.shortName = shortName;
+    this.race = race;
+    this.origin = origin;
+    this.attack = attack;
+    this.defense = defense;
+  }
+  
+  Character.prototype.describe = function() {
+    let sentence = `${this.name} is a ${this.race} from ${this.origin}`;
+    this.weapon ? console.log(`${sentence} who uses ${this.weapon}`) : console.log(sentence)
+  };
+  
+  Character.prototype.Fight = function(character) {
+    let x = this.attack - character.defense;
+    let y = character.attack - this.defense;
+    if (x < 0) x = 0;
+    if (y < 0) y = 0;
+    return `Your opponent takes ${x} damage and you receive ${y} damage`;
+  };
+  
+  Character.prototype.equipWeapon = function(weapon) {
+    this.weapon = weapon;
+  };
+  
+  
+  let characters = [
+    new Character('Gandalf the White','gandalf', 'Wizard', 'Middle Earth', 25, 15),
+    new Character('Gimli', 'Gimli', 'Dwarf', 'The house of Durin', 12, 16),
+    new Character('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 14, 18),
+    new Character('Aragorn son of Arathorn', 'aragorn', 'Man', 'Dunnedain', 35, 30),
+    new Character('Legolas', 'legolas', 'Wood Elf', 'Woodland Realm', 34, 36)
+  ];
+  
+  characters.push(new Character('Sauron', 'Sauron', 'Necromancer', 'Mordor', 50, 34,));
+  
+  characters.find(name => name.shortName === 'aragorn').describe();
+  // console.log(characters);
+  
+  let hobbits = characters.filter(name => name.race === 'Hobbit');
+  // console.log(hobbits);
+  
+  let higherThanFive = characters.filter(name => name.attack > 5);
+  // console.log(higherThanFive);
+  
+  characters.find(name => name.shortName === 'aragorn').equipWeapon('Anduril');
+  characters.find(name => name.shortName === 'aragorn').describe();
+
+  console.log(characters[1]);
